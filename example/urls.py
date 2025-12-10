7874ba087a3996284c77bae1287ee2950c7baa97
Mac GunJon <155982830+arkabarua69@users.noreply.github.com>
2025-12-10 15:05:43 +0600

    first commit0

--- example/urls.py
+++ example/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from example import views
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = "downloader"
 
@@ -9,4 +12,5 @@ urlpatterns = [
     path("contact/", views.contact_view, name="contact"),
     path("download/video", views.download_video, name="download_video"),
     path("download/audio", views.download_audio, name="download_audio"),
-]
+    path("favicon.ico", RedirectView.as_view(url="/static/favicon.ico")),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
